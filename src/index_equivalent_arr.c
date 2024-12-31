7de4171a40eba664bf0b6b332d6b2c3bf254c38c
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-12-31 13:37:47 +0100

    fix(index equivalent arr): fix bugs and memory leaks

--- src/index_equivalent_arr.c
+++ src/index_equivalent_arr.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 15:36:51 by damateos          #+#    #+#             */
-/*   Updated: 2024/12/31 12:24:36 by damateos         ###   ########.fr       */
+/*   Updated: 2024/12/31 13:33:00 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	*create_descending_sorted_arr(int *arr, size_t len)
 	sorted_arr = ft_int_arr_copy(arr, len);
 	if (!sorted_arr)
 		return (NULL);
-	ft_int_arr_reverse(sorted_arr, len);
 	ft_quick_sort(sorted_arr, 0, len - 1);
+	ft_int_arr_reverse(sorted_arr, len);
 	return (sorted_arr);
 }
 
@@ -30,6 +30,7 @@ t_hashmap	*create_number_to_index_map(int *arr, size_t len)
 	t_hashmap	*hm;
 	int			i;
 	t_hm_node	*node;
+	char		*key;
 
 	hm = ft_hm_create(len);
 	if (!hm)
@@ -37,7 +38,9 @@ t_hashmap	*create_number_to_index_map(int *arr, size_t len)
 	i = 0;
 	while (i < len)
 	{
-		node = ft_hm_node_insert(hm, ft_itoa(arr[i]), &i, sizeof(int));
+		key = ft_itoa(arr[i]);
+		node = ft_hm_node_insert(hm, key, &i, sizeof(int));
+		free(key);
 		if (!node)
 			return (ft_hm_remove(hm), NULL);
 		i++;
@@ -52,17 +55,20 @@ int	*get_index_equivalent_arr(int *arr, size_t len)
 	t_hashmap	*hm;
 	int			*temp_arr;
 	int			i;
+	char		*key;
 
 	temp_arr = create_descending_sorted_arr(arr, len);
 	if (!temp_arr)
 		return (NULL);
-	hm = create_number_to_index_map(arr, len);
+	hm = create_number_to_index_map(temp_arr, len);
 	if (!hm)
 		return (ft_free((void **)&temp_arr), NULL);
 	i = 0;
 	while (i < len)
 	{
-		temp_arr[i] = *(int *)ft_hm_get(hm, ft_itoa(arr[i]));
+		key = ft_itoa(arr[i]);
+		temp_arr[i] = *(int *)ft_hm_get(hm, key);
+		free(key);
 		i++;
 	}
 	ft_hm_remove(hm);
